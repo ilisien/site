c9b4b7d4b9bd86171e625e2f568f8b3a8c0f3ddc
ilisien <ian8lisien@gmail.com>
2026-02-23 15:23:56 +0000

    Feb 23, 2026, 10:23 AM

--- app.py
+++ app.py
@@ -2,12 +2,6 @@ from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
-url_for('static', filename='style.css')
-
 @app.route("/")
 def home():
-    return render_template()
-
-if __name__ == "__main__":
-    with app.app_context():
-        app.run(debug=True)
+    return render_template("home.html")
