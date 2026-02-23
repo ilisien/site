72bf1123cbb8f3e2a8fbc9d218f22b4ffb3a4b0d
ilisien <ian8lisien@gmail.com>
2026-02-23 15:20:54 +0000

    Feb 23, 2026, 10:20 AM

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ url_for('static', filename='style.css')
 @app.route("/")
 def home():
     return render_template()
+
+if __name__ == "__main__":
+    with app.app_context():
+        app.run(debug=True)
